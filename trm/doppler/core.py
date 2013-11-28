23ea2e17851e20169c7f1b3df297b598a7c285ab
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-11-28 17:04:29 +0000

    core.py -- moved check routine into this file

--- trm/doppler/core.py
+++ trm/doppler/core.py
@@ -6,6 +6,20 @@ Doppler tomography package
 Core classes & routines
 """
 
+def sameDims(arr1, arr2):
+    """
+    Checks that two numpy arrays have the same number of dimensions
+    and the same dimensions
+    """
+    if arr1.ndim != arr2.ndim:
+        return False
+
+    for d1,d2 in zip(arr1.shape, arr2.shape):
+        if d1 != d2:
+            return False
+
+    return True
+
 class DopplerError(Exception):
     pass
 
