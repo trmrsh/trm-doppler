202b1f5239efa5bde1cf8d11705481cb9219f273
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-11-29 19:41:21 +0000

    __init__.py -- into shape for installation
    Loads in various components.

--- trm/doppler/__init__.py
+++ trm/doppler/__init__.py
@@ -5,7 +5,11 @@ Doppler tomography package
 
 This is an implementation of Doppler tomography in Python.
 """
+from __future__ import absolute_import
 
-class DopplerError(Exception):
-    pass
+from .core import *
+from .data import *
+from .map  import *
+from ._doppler import *
 
+__all__ = ['Image', 'Map', 'Spectra', 'Data']
