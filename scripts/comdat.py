70dd2cd36e10927ec6e73a5c00b5834979c32bec
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-12-10 10:28:06 +0000

    comdat.py -- changed arguments and code
    Looks as though it works.

--- scripts/comdat.py
+++ scripts/comdat.py
@@ -15,44 +15,22 @@ from trm import doppler
 parser = argparse.ArgumentParser(description=usage)
 
 # positional
-parser.add_argument('map', help='name of the input map')
+parser.add_argument('map',   help='name of the input map')
+parser.add_argument('dtemp', help='data template file')
+parser.add_argument('dout',  help='data output file')
 
 # optional
-parser.add_argument('-n', dest='noise', store_action=True, help='add noise')
-parser.add_argument('-r', dest='read', type=float, default=10., help='RMS read noise per pixel.')
-parser.add_argument('-g', dest='gain', type=float, default=1.0, help='gain, electrons/unit')
-parser.add_argument('-c', dest='cont', type=float, default=1.0, help='effective continuum level')
-
+parser.add_argument('-n', dest='noise', action='store_true', help='add noise')
 
 # OK, done with arguments.
 args = parser.parse_args()
 
+# load map and data
+dmap  = doppler.Map.rfits(doppler.afits(args.map))
+dtemp = doppler.Data.rfits(doppler.afits(args.dtemp))
 
+# compute data
+dout  = doppler.comdat(dmap, dtemp)
 
-    # associate with one wavelength
-    wave2   = 468.6
-    gamma2  = 150.
-    def2    = doppler.Default.gauss2d(300.)
-
-    # create Image
-    image2  = doppler.Image(array2, vxy, wave2, gamma2, def2)
-
-    # ephemeris etc
-    tzero  = 50000.
-    period = 0.1
-    vfine  = 20.
-    vpad   = 700.
-
-    # create the Map
-    map = doppler.Map(mhead,[image1,image2],tzero,period,vfine,vpad)
-    print 'created map'
-
-    # Write to a fits file
-    map.wfits('map.fits')
-
-    # Calculate the default
-    mdef = doppler.comdef(map)
-
-    # Write the result to a FITS file
-    mdef.wfits('def.fits')
-
+# Write to a fits file
+dout.wfits(doppler.afits(args.dout))
