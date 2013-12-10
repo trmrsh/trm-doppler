3487f367549498b9c502ba34d66a710397be1c8e
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-12-10 10:27:23 +0000

    fakedata.py -- changed some defaults.
    The times had silly values by default.

--- scripts/fakedata.py
+++ scripts/fakedata.py
@@ -21,9 +21,9 @@ parser.add_argument('fake', help='name of the output data file')
 # optional
 parser.add_argument('-w1', type=float, default=420., help='Minimum wavelength')
 parser.add_argument('-w2', type=float, default=520., help='Maximum wavelength')
-parser.add_argument('-nw', type=int,   default=1000, help='Number of wavelengths')
-parser.add_argument('-t1', type=float, default=420., help='First time')
-parser.add_argument('-t2', type=float, default=520., help='Last time')
+parser.add_argument('-nw', type=int,   default=1200, help='Number of wavelengths')
+parser.add_argument('-t1', type=float, default=50000.0, help='First time')
+parser.add_argument('-t2', type=float, default=50000.1, help='Last time')
 parser.add_argument('-ns', type=int,   default=40,   help='Number of spectra')
 parser.add_argument('-e', dest='error', type=float, default=1.0, help='RMS noise level per pixel')
 parser.add_argument('-f', dest='fwhm', type=float, default=200., help='FWHM resoluton (km/s)')
@@ -40,8 +40,6 @@ wave = np.linspace(args.w1, args.w2, args.nw)
 time = np.linspace(args.t1, args.t2, args.ns)
 
 wave, ignore = np.meshgrid(wave, time)
-
-shape  = (10,100)
 flux   = np.zeros_like(wave,dtype=np.float32)
 ferr   = args.error*np.ones_like(flux)
 expose = (time[-1]-time[0])/(len(time)-1)*np.ones_like(time, dtype=np.float32)
@@ -55,12 +53,5 @@ spectra = doppler.Spectra(flux,ferr,wave,time,expose,ndiv,fwhm)
 data = doppler.Data(head,spectra)
 
 # Write to a fits file
-if args.fake.endswith('.fits') or args.fake.endswith('.fits') or \
-        args.fake.endswith('.fits.gz') or args.fake.endswith('.fit.gz'):
-    oname = args.fake
-else:
-    oname = args.fake + '.fits'
-
-data.wfits(oname)
-
+data.wfits(doppler.afits(args.fake))
 
