be0bdb681a6a8902b574fb8bc6c123908b112b74
Tom Marsh <t.r.marsh@warwick.ac.uk>
2013-12-10 18:34:58 +0000

    comdat.py -- added noise section
    Now can add noise.

--- scripts/comdat.py
+++ scripts/comdat.py
@@ -20,7 +20,7 @@ parser.add_argument('dtemp', help='data template file')
 parser.add_argument('dout',  help='data output file')
 
 # optional
-parser.add_argument('-n', dest='noise', action='store_true', help='add noise')
+parser.add_argument('-n', dest='noise', action='store_true', help='add noise according to uncertainty array in template')
 
 # OK, done with arguments.
 args = parser.parse_args()
@@ -31,6 +31,11 @@ dtemp = doppler.Data.rfits(doppler.afits(args.dtemp))
 
 # compute data
 dout  = doppler.comdat(dmap, dtemp)
+
+# optionally add noise
+if args.noise:
+    for spectra in dout.data:
+        spectra.flux = np.random.normal(spectra.flux, spectra.ferr)
 
 # Write to a fits file
 dout.wfits(doppler.afits(args.dout))
