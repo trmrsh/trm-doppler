3ce78a2654e1c7b8d8702a58e9f65c43a0e402e1
Tom Marsh <t.r.marsh@warwick.ac.uk>
2014-01-03 11:11:37 +0000

    svd.py, svdfit.py -- two new scripts for distribution
    Carry out SVD-based analysis

---
+++ scripts/svd.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+usage = \
+"""
+Singular value decomposition routine. This one generates a FITS file
+containing all the SVD vectors, each of which is a square image matching the
+input Grid. It returns this as a 3D array which can be conveniently examined
+with ds9. For an NxN grid, there are N**2 possible SVD vectors so the array
+has C-style dimensions (N*N) x N x N = N**4, and one should therefore not go
+mad with overlay large N. Use is mainly educational.
+"""
+
+import argparse
+import numpy as np
+from trm import doppler
+from astropy.io import fits
+
+parser = argparse.ArgumentParser(description=usage)
+
+# positional
+parser.add_argument('grid',  help='name of the input grid')
+parser.add_argument('data',   help='data file')
+parser.add_argument('svd',  help='name of the SVD output file')
+
+# optional
+parser.add_argument('-n', dest='ntdiv', type=int,
+                    default=11, help='spectrum sub-division factor')
+
+# OK, done with arguments.
+args = parser.parse_args()
+
+# load grid and data
+grid = doppler.Grid.rfits(doppler.afits(args.grid))
+data = doppler.Data.rfits(doppler.afits(args.data))
+
+# generate the matrices
+A, b = doppler.genmat(grid, data, args.ntdiv)
+
+# Carry out full SVD, returning smallest matrices possible
+u, s, v = np.linalg.svd(A,full_matrices=False)
+
+ng = grid.data.shape[0]
+# Matrix v is the one we want. It should have dimensions (ng*ng) x (ng*ng) where
+# N is number along each side of the grids. Need to re-shape
+v = np.reshape(v, (ng*ng,ng,ng))
+
+# now write to FITS
+
+head = fits.Header()
+head['SOURCE'] = 'svd.py'
+hdu = fits.PrimaryHDU(data=v, header=head)
+hdu.writeto(doppler.afits(args.svd))
+
---
+++ scripts/svdfit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+usage = \
+"""
+singular value decomposition. Computes the Grid that best matches a Data,
+allowing the user to control the number of singular values used with the
+parameter 'cond'. If cond < 1, then only singular values greater than cond as
+a fraction of the highest are used. If cond >=1 then it is rounded to the
+nearest integer and used as the number of the highest singular values to use.
+The maximum possible value equals the number of points in the grid.
+"""
+
+import argparse
+from trm import doppler
+
+parser = argparse.ArgumentParser(description=usage)
+
+# positional
+parser.add_argument('igrid',  help='name of the input grid')
+parser.add_argument('data',   help='data file')
+parser.add_argument('cond',   type=float, help='SVD conditioning parameter')
+parser.add_argument('ogrid',  help='name of the output grid')
+
+# optional
+parser.add_argument('-n', dest='ntdiv', type=int,
+                    default=11, help='spectrum sub-division factor')
+
+# OK, done with arguments.
+args = parser.parse_args()
+
+# load grid and data
+grid = doppler.Grid.rfits(doppler.afits(args.igrid))
+data = doppler.Data.rfits(doppler.afits(args.data))
+
+chisq, cred, sing, s, x = doppler.svd(grid, data, args.cond, args.ntdiv, True)
+
+grid.data = x[0]
+grid.wfits(doppler.afits(args.ogrid))
+
+print 'Chi**2        =',chisq[0]
+print 'Chi**2 / Ndof =',cred[0]
